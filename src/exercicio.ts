df8303f8f9f52460ea920d50c92b44456f896337
michel997 <gekhanom@outlook.com>
2023-08-30 11:31:00 -0300

    exercicio com função multiplicar e saudação

---
+++ src/exercicio.ts
@@ -0,0 +1,14 @@
+//Função de multiplicação
+function multiplicar (x: number, y: number): number {
+    return x * y;
+}
+let resultado1 = multiplicar(10, 5)
+console.log(resultado1);
+
+
+//Função de saudação
+function ola (nome: string ) {
+    return "Olá " + nome
+}
+let resultado2 = ola("Michel")
+console.log(resultado2);
